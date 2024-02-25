69313a3f6fbb65a705364f63212dbc7e0a2cfc85
Yuxin <yuxin_ye@protonmail.com>
2024-02-25 19:00:55 +1100

    finish insert

--- insert.js
+++ insert.js
@@ -7,6 +7,8 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-let { data: posts, error } = await supabase.from("posts").select("*");
+let { data: posts, error } = await supabase
+  .from("posts")
+  .insert([{ user_name: "CM", body: "Insert from the JS client" }]);
 
 console.log(posts);
